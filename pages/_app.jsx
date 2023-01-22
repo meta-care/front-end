2216cf9dd06827e4078197c4f97edbc970067201
N0E-P <pecci.noe@gmail.com>
2023-01-22 16:19:34 +0100

    no more hydration errors & images cleaning

---
+++ pages/_app.jsx
@@ -0,0 +1,44 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+	RainbowKitProvider,
+	getDefaultWallets,
+	connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider, webSocketProvider } = configureChains(
+	[chain.goerli, chain.mainnet],
+	[
+		alchemyProvider({
+			// This is Alchemy's default API key.
+			// You can get your own at https://dashboard.alchemyapi.io
+			apiKey: "_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC",
+		}),
+		publicProvider(),
+	]
+);
+
+const { connectors } = getDefaultWallets({
+	appName: "RainbowKit App",
+	chains,
+});
+
+const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider,
+	webSocketProvider,
+});
+
+export default function MyApp({ Component, pageProps }) {
+	return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider chains={chains}>
+				<Component {...pageProps} />
+			</RainbowKitProvider>
+		</WagmiConfig>
+	);
+}
