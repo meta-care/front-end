25ab5fc60866ceb0a10cf3ab2ec429145c99dc73
DarkKolp <nunoandresilva199@gmail.com>
2023-07-16 14:30:39 +0100

    newsletter form

--- components/home/Body/SubscribeForm.jsx
+++ components/home/Body/SubscribeForm.jsx
@@ -1,17 +1,44 @@
 import styles from "./body.module.css";
+import { useState } from "react";
+// Example taken from: https://codewithmarish.com/post/newsletter-subscription-using-next-js-and-mailchimp-api
 
-//We should preform the backend to store the user's email
+
 
 const SubscribeForm = () => {
+    const [state, setState] = useState(0);
+  const [errorMsg, setErrorMsg] = useState("");
+  // 0 - initial , 1 - loading, 2 - success, 2 - error
+const subscribe = async (e) => {
+    e.preventDefault();
+
+    setState(1);
+    setErrorMsg("");
+    console.log(e.target[0].value);
+    try {
+      const res = await fetch("../../api/newsletter", {
+        method: "POST",
+        body: e.target[0].value,
+      });
+
+      const data = await res.json();
+      if (data.error !== null) {
+        throw data.error;
+      }
+      setState(2);
+    } catch (e) {
+        setErrorMsg(e);
+      setState(3);
+    }
+  };
     return ( 
-        <div className={styles.waitlistForm}>
+        <form onSubmit={subscribe} className={styles.waitlistForm}>
             <input type="email" placeholder="Enter your email"
             className={styles.inputWaitlist}/>
             <button type="submit"
             className={styles.button}>
-                Submit
+                Subscribe
             </button>
-        </div>
+        </form>
      );
 }
  
