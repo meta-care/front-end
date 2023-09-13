9ad5e51b3051ea5727f462b323dfe34aa50068f5
N0E-P <pecci.noe@gmail.com>
2023-09-13 23:04:33 +0200

    Save the AvatarURL to the database

--- pages/createAvatar.jsx
+++ pages/createAvatar.jsx
@@ -1,29 +1,96 @@
-import { AvatarCreator, AvatarCreatorConfig, AvatarExportedEvent } from '@readyplayerme/react-avatar-creator';
+import {
+	AvatarCreator,
+	AvatarCreatorConfig,
+	AvatarExportedEvent,
+} from "@readyplayerme/react-avatar-creator";
+import { getUser } from "../components/mongoDB/getUser";
+import { getSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const config = {
-  clearCache: true,
-  bodyType: 'fullbody',
-  quickStart: false,
-  language: 'en',
+	clearCache: true,
+	bodyType: "fullbody",
+	quickStart: false,
+	language: "en",
 };
 
 const style = {
-  width: '100%',
-  height: '100vh',
-  border: 'none',
+	width: "100%",
+	height: "100vh",
+	border: "none",
 };
 
-export default function CreateAvatar() {
-  const handleOnAvatarExported = (event) => {
-    console.log(`Avatar URL is: ${event.data.url}`);
-  };
+export default function CreateAvatar({ user }) {
+	const [avatarCreated, setAvatarCreated] = useState(false);
+	const [avatarURL, setAvatarURL] = useState("");
+	const [saved, setSaved] = useState(false);
 
-  return (
-    <AvatarCreator
-      subdomain="nunos"
-      config={config}
-      style={style}
-      onAvatarExported={handleOnAvatarExported}
-    />
-  );
+	const handleOnAvatarExported = async (event) => {
+		setAvatarURL(event.data.url);
+		setAvatarCreated(true);
+		setSaved(false);
+	};
+
+	// Update the user profile
+	useEffect(() => {
+		if (avatarCreated) {
+			console.log("creating avatar");
+			fetch("/api/saveAvatar", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ email: user.email, avatarURL }),
+			})
+				.then((response) => {
+					return response.json();
+				})
+				.then((data) => {
+					setSaved(true);
+					setAvatarCreated(false);
+				})
+				.catch((error) => {
+					console.error(error);
+				});
+		}
+	}, [avatarCreated]);
+
+	return (
+		<AvatarCreator
+			subdomain="nunos"
+			config={config}
+			style={style}
+			onAvatarExported={handleOnAvatarExported}
+		/>
+	);
+}
+
+export async function getServerSideProps(context) {
+	// Check if the user is connected. Otherwise return him to the home page
+	const session = await getSession(context);
+	if (!session) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		};
+	}
+
+	// Get the user profile
+	const profile = await getUser(session);
+	if (!profile) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		};
+	}
+	const user = JSON.parse(JSON.stringify(profile));
+
+	// Return the user profile
+	return {
+		props: { user },
+	};
 }
